fbd983508a8ff152e95b80be634f4815fadad51f
shiv <shiv>
2016-08-03 01:01:17 +0530

    s

---
+++ public/javascripts/app.js
@@ -0,0 +1,22 @@
+//Define an angular module for our app
+var sampleApp = angular.module('sampleApp', []);
+
+//Define Routing for app
+//Uri /AddNewOrder -> template add_order.html and Controller AddOrderController
+//Uri /ShowOrders -> template show_orders.html and Controller AddOrderController
+sampleApp.config(['$routeProvider',
+  function($routeProvider) {
+    $routeProvider.
+      when('/AddNewOrder', {
+		templateUrl: 'templates/add_order.html',
+		controller: 'AddOrderController'
+	}).
+      when('/ShowOrders', {
+		templateUrl: 'templates/show_orders.html',
+		controller: 'ShowOrdersController'
+      }).
+      otherwise({
+		redirectTo: '/ShowOrders'
+      });
+}]);
+
